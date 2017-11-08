4461d0205c22908073efcf763c0b2a5102a7fa0c
Douglas Alpuche <m190108@usna.edu>
2017-11-08 16:05:51 -0500

    Test- Update of generate_pdf

--- generate_pdf.php
+++ generate_pdf.php
@@ -17,17 +17,29 @@
     <h1>Page Title</h1>
     <a href="./">Lab Homepage</a>
     <?php
-require('./fpdf181/fpdf.php');
-require('./fpdi/src/autoload.php');
+#DEBUG
 
-$filename='./export.pdf';
+use setasign\Fpdi\Fpdi;
+use setasign\Fpdi\PdfReader;
 
-$template_pdf=new FPDI();
+include('./includes/php/error.inc.php');
+require_once('./includes/fpdf181/fpdf.php');
+require_once('./includes/fpdi/src/autoload.php');
+    
+    
+$filename='./blank_chit.pdf';
+
+$template_pdf=new Fpdi();
 $template_pdf->AddPage();
 $template_pdf->setSourceFile($filename);
-$template_pdf->importPage(1);
-$template_pdf->useTemplate()
+$template_index=$template_pdf->importPage(1);
+$template_pdf->useTemplate($template_index,10,10,100);
+
+$template_pdf->SetFont('Helvetica');
+$template_pdf->SetTextColor(255, 0, 0);
+$template_pdf->SetXY(30, 30);
+$template_pdf->Write(0, 'This is just a simple text');
+
+$template_pdf->Output("D","Chit.pdf");            
+
 ?>
-    </div>
-  </body>
-</html>
