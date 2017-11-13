b6167d3af1039e372c0e76885983dac863391ee6
reilers <Ryan.Eilers1197@gmail.com>
2017-11-13 10:26:45 -0500

    =updated stuff

--- newChit.php
+++ newChit.php
@@ -55,62 +55,95 @@
                         <td><input type="text" class="form-control" id="toBillet" name="toBillet" value=""></td>
                     </tr>
                   </div>
+                  <tr><td><br></td></tr>
                   <div class="form-group">
                     <tr>
                         <td colspan="3"><strong>Your Information</strong></td>
                     </tr>
-                    <tr>
-                        <td><label for="MIDNname" class="control-label">&nbsp;Name:&nbsp;</label></td>
-                        <td><input type="text" class="form-control" id="MIDNname" name="MIDNname" value=""></td>
-                        <td><label for="MIDNrank" class="control-label">&nbsp;Rank:&nbsp;</label></td>
+                    <div class="input-group">
+                      <tr>
+                          <td><label for="MIDNname" class="control-label">&nbsp;Name:&nbsp;</label></td>
+                          <td><input type="text" class="form-control" id="MIDNname" name="MIDNname" value=""></td>
+                          <td><label for="MIDNrank" class="control-label">&nbsp;Rank:&nbsp;</label></td>
+                          <td>
+                            <select class="form-control" id="MIDNrank" name="MIDNrank">
+                              <option value="NULL"></option>
+                              <option value="4">4/C</option>
+                              <option value="3">3/C</option>
+                              <option value="2">2/C</option>
+                              <option value="1">1/C</option>
+                            </select>
+                          </td>
+                          <td><label for="MIDNalpha" class="control-label">&nbsp;Alpha:&nbsp;</label></td>
+                          <td><input type="number" class="form-control" id="MIDNalpha" name="MIDNalpha" value=""></td>
+                      </tr>
+                    </div>
+                    <div class="input-group">
+                      <tr>
+                        <td><label for="MIDNclassyr">&nbsp;Graduating Year:&nbsp;</label></td>
                         <td>
-                          <select class="form-control" id="MIDNrank" name="MIDNrank">
+                          <select class="form-control" id="MIDNclassyr" name="MIDNclassyr">
                             <option value="NULL"></option>
-                            <option value="4">4/C</option>
-                            <option value="3">3/C</option>
-                            <option value="2">2/C</option>
-                            <option value="1">1/C</option>
+                            <?php
+                              for ($i=2018; $i < 2022; $i++) {echo "<option value='$i'>$i</option>";}
+                             ?>
                           </select>
                         </td>
-                        <td><label for="MIDNalpha" class="control-label">&nbsp;Alpha:&nbsp;</label></td>
-                        <td><input type="number" class="form-control" id="MIDNalpha" name="MIDNalpha" value=""></td>
-                    </tr>
-                    <tr>
-                      <td><label for="MIDNclassyr">&nbsp;Graduating Year:&nbsp;</label></td>
-                      <td>
-                        <select class="form-control" id="MIDNclassyr" name="MIDNclassyr">
-                          <option value="NULL"></option>
-                          <?php
-                            for ($i=2018; $i < 2022; $i++) {echo "<option value='$i'>$i</option>";}
-                           ?>
-                        </select>
-                      </td>
-                      <td><label for="MIDNcompany">&nbsp;Company:&nbsp;</label></td>
-                      <td>
-                        <select class="form-control" id="MIDNcompany" name="MIDNcompany">
-                          <option value="NULL"></option>
-                          <?php
-                            for ($i=1; $i < 31; $i++) {echo "<option value='$i'>$i</option>";}
-                           ?>
-                        </select>
-                      </td>
-                      <td><label for="MIDNroom">Room Number:&nbsp;</label></td>
-                      <td><input type="number" class="form-control" id="MIDNroom" name="MIDNroom" value=""></td>
-                    </tr>
-                    <tr>
-                      <td><label for="MIDNSQPR"> SQPR:&nbsp;</label></td>
-                      <td><input type="number" class="form-control" id="MIDNSQPR" name="MIDNSQPR" value=""></td>
-                      <td><label for="MIDNCQPR"> CQPR:&nbsp;</label></td>
-                      <td><input type="number" class="form-control" id="MIDNCQPR" name="MIDNCQPR" value=""></td>
-                      <td><label for="MIDNPerf"> Performance Grade:&nbsp;</label></td>
-                      <td><input type="text" class="form-control" id="MIDNPerf" name="MIDNPerf" value=""></td>
-                    </tr>
+                        <td><label for="MIDNcompany">&nbsp;Company:&nbsp;</label></td>
+                        <td>
+                          <select class="form-control" id="MIDNcompany" name="MIDNcompany">
+                            <option value="NULL"></option>
+                            <?php
+                              for ($i=1; $i < 31; $i++) {echo "<option value='$i'>$i</option>";}
+                             ?>
+                          </select>
+                        </td>
+                        <td><label for="MIDNroom">Room Number:&nbsp;</label></td>
+                        <td><input type="number" class="form-control" id="MIDNroom" name="MIDNroom" value=""></td>
+                      </tr>
+                    </div>
+                    <div class="input-group">
+                      <tr>
+                        <td><label for="MIDNSQPR"> SQPR:&nbsp;</label></td>
+                        <td><input type="number" class="form-control" id="MIDNSQPR" name="MIDNSQPR" value=""></td>
+                        <td><label for="MIDNCQPR"> CQPR:&nbsp;</label></td>
+                        <td><input type="number" class="form-control" id="MIDNCQPR" name="MIDNCQPR" value=""></td>
+                        <td><label for="MIDNPerf"> Performance Grade:&nbsp;</label></td>
+                        <td>
+                          <select class="form-control" id="MIDNPerf" name="MIDNPerf">
+                            <option value="NULL"></option>
+                            <?php
+                              for ($i='A'; $i < 'G'; $i++) {echo "<option value='$i'>$i</option>";}
+                             ?>
+                          </select>
+                        </td>
+                      </tr>
+                    </div>
+                    <div class="input-group">
+                      <tr>
+                        <td><label for="MIDNCond">Conduct Grade:&nbsp;</label></td>
+                        <td>
+                          <select class="form-control" id="MIDNCond" name="MIDN">
+                            <option value="NULL"></option>
+                            <?php
+                              for ($i='A'; $i < 'G'; $i++) {echo "<option value='$i'>$i</option>";}
+                             ?>
+                          </select>
+                        </td>
+                      </tr>
+                    </div>
+
                     <tr>
                       <td><label for="MIDNremarks">Remarks</label></td>
                       <td><textarea name="MIDNremarks" class="form-control" id="MIDNremarks" rows="8" cols="80"></textarea></td>
                     </tr>
                   </div>
-
+                  <tr><td><br></td></tr>
+                  <div class="form-group">
+                    <tr>
+                      <td><label for="Ref">References: &nbsp;</label></td>
+                    </tr>
+                  </div>
                   <tr>
                     <td>
                       <input type="button" name="submit" value="submit">
