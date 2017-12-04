bf0f3222e4fc1ce1ba710be37641df5af59a6879
Xu <m197116@usna.edu>
2017-12-04 00:11:09 -0500

    Hopefully this doesn't overwrite your shit

--- login/register-user.php
+++ login/register-user.php
@@ -25,37 +25,10 @@
   </head>
 
   <body>
-<nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="#">E-Chits</a>
-    </div>
-
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav navbar-right">
-<li><form class="navbar-form navbar-right" action="#" method="POST">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Search">
-        </div>
-        <button type="submit" class="btn btn-default">Find Chit</button>
-      </form>
-</li>  
-        <li><a href="./AllChits.php">View All Chits</a></li>
-        <?php if(isset($_SESSION['username'])) echo "<li><a href=\"./logout.php\">Logout</a></li>"; ?>
-</ul>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
 
 <?php
+  require('../nav.inc.php');
+  nav(1);
 // include "login-user.php";
 
 // PLAN ON REMOVING THE OPTION TO ALLOW USERS TO CREATE OWN USERNAME. ONLY
