49e8a2dd0f44e99651a9a325f38171c86068c67d
Scott Mayer <m183990@usna.edu>
2017-12-14 19:00:56 -0500

    added stuff

--- viewchit.php
+++ viewchit.php
@@ -68,13 +68,6 @@ else{
 
 ?>
 <div class="container courier">
-  <div class="row">
-    <div class="col-md-12">
-      <div class="page-header">
-        <h1><em>View Chit</em></h1>
-      </div>
-    </div>
-  </div>
 
   <div id="banner">
 
@@ -1423,8 +1416,41 @@ else{
   <div class="col-sm-4">
   </div>
   <div class="col-sm-4 text-center">
-    <button class="btn btn-primary">Edit Chit</button>
-    <button class="btn btn-danger">Delete Chit</button>
+		<button type="button" class="btn btn-danger" data-toggle="modal" data-target="#myModal">Delete</button>
+
+		<!-- Modal -->
+		<div id="myModal" class="modal fade" role="dialog">
+			<div class="modal-dialog">
+
+				<!-- Modal content-->
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal">&times;</button>
+						<h4 class="modal-title">Are you sure you want to delete this chit?</h4>
+					</div>
+					<div class="modal-footer">
+
+						<div class="col-xs-6 text-left">
+							<div class="previous">
+								<button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+							</div>
+						</div>
+						<div class="col-xs-6 text-right">
+							<div class="next">
+
+								<form action="deletechit.php" method="post">
+									<input type="submit" class="btn btn-danger" data-dismiss="modal" value="Delete">
+								</form>
+							</div>
+						</div>
+
+
+					</div>
+				</div>
+
+			</div>
+		</div>
+
   </div>
   <div class="col-sm-4">
   </div>
